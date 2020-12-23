6b6c513bc6cf169618dfd086025c0a13905c7b97
Gabriel Nuñez <gabs88.nu@gmail.com>
2020-12-22 21:58:17 -0300

    second commit

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.render('index', {title: 'First Website'});
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {title: 'Contact Page'});
+});
+
+module.exports = router;
